806a77aa7285308aac79b702a8936f34ba4552bb
chirag-talpada <chiragvaghela492@gmail.com>
2022-08-07 19:11:15 +0530

    adding extra features

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 const cards=document.querySelectorAll('.card');
 let cardOne,cardTwo;
 let openCard=false;
-let match=0;
+let match=0,flip=0;
 const wrongAudio=new Audio('./effect/wrong_card.wav');
 const rightAudio=new Audio('./effect/right_card.wav');
 const winAudio=new Audio('./effect/win.mp3');
 
+const jsConfetti = new JSConfetti();
+
+async function hurrey(){
+    await jsConfetti.addConfetti({
+        emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+        confettiRadius: 6,
+        confettiNumber: 200,
+        emojiSize: 40
+    });
+     window.location.reload();
+}
+
 function flipCard({target: clickedCard}){
     
     if(clickedCard!=cardOne && !openCard){
+        
         clickedCard.classList.add("flip");
+        flip++;
+        document.getElementById('flipscore').innerText="flips:"+flip;
         if(!cardOne){
             return cardOne=clickedCard;
         }        
+        rightAudio.pause();
+        rightAudio.currentTime = 0;
+        
         cardTwo=clickedCard;
         openCard=true;
         macthcard(cardOne.querySelector('img').src,cardTwo.querySelector('img').src);
@@ -23,16 +41,20 @@ function flipCard({target: clickedCard}){
 function macthcard(img1,img2){
     if(img1===img2){
         match++;
+
+        let per=(match*100)/8;
+        document.getElementById('percentage').innerText=Math.trunc(per)+"%";
+        document.querySelector('.fillbar').style.width=per+"%";
+
         if(match===8){
             setTimeout(()=>{
                 winAudio.play();
+                hurrey();
                 setTimeout(()=>{
                     winAudio.pause();
-                },3000);
+                },2000);
                 return shufflecard();
-            },1000)
-
-            
+            },1000)   
         }
         cardOne.removeEventListener("click",flipCard);
         cardTwo.removeEventListener("click",flipCard);
@@ -41,6 +63,7 @@ function macthcard(img1,img2){
         cardOne = cardTwo = "";  
         setTimeout(()=>{
             rightAudio.pause();
+            rightAudio.currentTime = 0;
             rightAudio.play();            
         },400);
         return openCard=false;
@@ -63,9 +86,13 @@ function macthcard(img1,img2){
 
 function shufflecard(){
     matched = 0;
+    flip = 0;
     openCard = false;
     cardOne = cardTwo = "";
-    
+    document.getElementById('flipscore').innerText="flips:" + flip;
+    document.getElementById('percentage').innerText="0%";
+    document.querySelector('.fillbar').style.width="0%";
+
     let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
     arr.sort(() => Math.random() > 0.5 ? 1 : -1);
     
@@ -81,5 +108,9 @@ shufflecard();
 
 cards.forEach(card => {
     card.addEventListener("click", flipCard);
+});
+
+document.querySelector('.replay').addEventListener("click",()=>{
+    shufflecard();
 });
 
